d3080885f91ce4dab2fe5f8c5c0e054577e9aa40
Ivona I <91659350+ivonaiva@users.noreply.github.com>
2022-06-21 14:15:30 +0200

    Ex4 new base.

--- src/main/java/at/ac/fhcampuswien/App.java
+++ src/main/java/at/ac/fhcampuswien/App.java
@@ -1,42 +1,10 @@
 package at.ac.fhcampuswien;
 
-import javafx.application.Application;
-import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.image.Image;
-import javafx.scene.paint.Color;
-import javafx.stage.Stage;
+import at.ac.fhcampuswien.ui.Menu;
 
-
-import java.io.File;
-import java.net.URL;
-
-public class App extends Application {
-@FXML
+public class App {
     public static void main(String[] args) {
-       launch(args);
-
-
-    }
-
-    @Override
-    public void start(Stage stage) throws Exception {
-        URL url = new File("src/main/resources/graphicalInterface.fxml").toURI().toURL();
-        Parent root = FXMLLoader.load(url);
-        stage.setTitle("_the news app_");
-        stage.getIcons().add(new Image("file:src/main/resources/icon.png"));
-        Scene scene = new Scene(root, 1000, 625);
-        stage.setScene(scene);
-        scene.setFill(Color.web("#db4840"));
-        stage.setResizable(false);
-
         Menu menu = new Menu();
         menu.start();
-
-
-        stage.show();
-
     }
 }
